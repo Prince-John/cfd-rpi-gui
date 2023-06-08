4efda58c6ebefa211ef6eda5e7332db99c1d7bd3
prince <prince.apple.john@gmail.com>
2023-06-08 17:31:43 -0500

    git fix

---
+++ src/C/i2c_test/i2c_wiring_mux_test.c
@@ -0,0 +1,19 @@
+#include <wiringPiI2C.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define Device_Address 0x70
+#define MUX_CH01 0b00000001
+#define MUX_CH02 0b00000010
+
+
+
+
+int main() {
+
+    fd = wiringPiI2CSetup(Device_Address);
+
+    wiringPiI2CWriteReg8 (fd, MUX_CH01);
+
+}
