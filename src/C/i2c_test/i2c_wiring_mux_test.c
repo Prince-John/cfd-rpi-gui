24120cacf4c37aca54f350c6d765b531f0e32d0b
prince <prince.apple.john@gmail.com>
2023-06-12 14:30:09 -0500

    mux test code

--- src/C/i2c_test/i2c_wiring_mux_test.c
+++ src/C/i2c_test/i2c_wiring_mux_test.c
@@ -9,6 +9,7 @@
 #define MUX_CH2 0b00000100
 
 int fd;
+int verbose =1;
 
 void ms_delay(int val){
     int i,j;
@@ -16,17 +17,58 @@ void ms_delay(int val){
         for(j=0;j<1200;j++);
 }
 
+void set_mux_channel(int fd, int channel){
+
+    switch (channel) {
+
+        case 0:
+            wiringPiI2CWrite (fd, MUX_CH0);
+            break;
+        case 1:
+            wiringPiI2CWrite (fd, MUX_CH1);
+            break;
+        case 2:
+            wiringPiI2CWrite (fd, MUX_CH2);
+            break;
+        case 3:
+            wiringPiI2CWrite (fd, MUX_CH3);
+            break;
+        case 4:
+            wiringPiI2CWrite (fd, MUX_CH4);
+            break;
+        case 5:
+            wiringPiI2CWrite (fd, MUX_CH5);
+            break;
+        case 6:
+            wiringPiI2CWrite (fd, MUX_CH6);
+            break;
+        case 7:
+            wiringPiI2CWrite (fd, MUX_CH7);
+            break;
+        default:
+            wiringPiI2CWrite (fd, MUX_CH0);
+            break;
+    }
+    if(verbose == 1){
+        printf("Setting i2c mux to channel %d", channel);
+    }
+    ms_delay(10);
+}
+
+
 
 int main(int argc, char **argv) {
 
     fd = wiringPiI2CSetup(Device_Address);
-    printf("fd is  %d \n",fd);
-    ms_delay(50);
-    ms_delay(50);
-    wiringPiI2CWrite (fd, MUX_CH01);
-    printf("writing to mux %d",MUX_CH01);
-    ms_delay(50);
-
+    fd1 = wiringPiI2CSetup(Device_Address+3);
+    set_mux_channel(fd,1);
+    wiringPiI2CWrite (fd1, 0xFF);
+    set_mux_channel(fd,2)
+    wiringPiI2CWrite (fd1, 0xFF);
+    set_mux_channel(fd,3)
+    wiringPiI2CWrite (fd1, 0xFF);
+    set_mux_channel(fd,4)
+    wiringPiI2CWrite (fd1, 0xFF);
 
 
 }
