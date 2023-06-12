2fb492b30f909026261c10cf74adf15b7d408cdc
prince <prince.apple.john@gmail.com>
2023-06-12 17:47:45 -0500

    bug fix

--- src/C/i2c_test/i2c_wiring_mux_test.c
+++ src/C/i2c_test/i2c_wiring_mux_test.c
@@ -74,7 +74,7 @@ int main(int argc, char **argv) {
 
     while (1){
 
-        if(fgets (input_str, 1, stdin) != NULL) {
+        if(fgets (input_str, 2, stdin) != NULL) {
             /* writing content to stdout */
             printf("%s", input_str);
         }
